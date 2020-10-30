df6dc9f285f9100c5ba0b1743b48c3f091317498
Sharif <srfsof23@gmail.com>
2020-10-30 08:39:04 +0600

    This is my right function on reverse

---
+++ reverse.go
@@ -0,0 +1,9 @@
+package sharif
+
+func sharif(sharifa string) string {
+	runes := []rune(sharifa)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
